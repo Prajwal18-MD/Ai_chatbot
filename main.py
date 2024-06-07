127318344593ff4a30a2cff9ed72bd152fd00e78
Prajwal-18MD <prajju.18gryphon@gmail.com>
2024-06-07 10:12:25 +0530

    commit

--- main.py
+++ main.py
@@ -11,4 +11,16 @@ import json
 with open("intents.json") as file:
     data = json.load(file)
     
-print(data)
+words = []
+labels = []
+docs = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs.append(pattern)
+        
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+        
