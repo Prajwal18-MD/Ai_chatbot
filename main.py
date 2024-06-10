cb64aebcfdfebb1db67928c0df44ef02bc0e4da8
Prajwal-18MD <prajju.18gryphon@gmail.com>
2024-06-10 11:12:05 +0530

    chat

--- main.py
+++ main.py
@@ -78,3 +78,37 @@ try:
 except:
     model.fit(training, output, n_epoch=1000, batch_size=8, show_metric = True)
     model.save("model.tflearn")
+    
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+    
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+    
+    for se in s_words:
+        for i,w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+    
+    return np.array(bag)
+
+
+def chat():
+    print("Hey! wanna Know about our Resturant (type quit to end!)")
+    while True:
+        inp = input("You : ")
+        if inp.lower() == 'quit':
+            break
+        
+        results = model.predict([bag_of_words(inp, words)])
+        results_index = np.argmax(results)
+        tag = labels[results_index]
+        
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+        print(random.choice(responses))
+
+chat()
+        
